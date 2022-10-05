724daf2c1af83ce0167ae4ec494b52f292c59e19
Lee <2544510157@qq.com>
2022-10-05 20:47:46 +0800

    chore: 补充eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended' 
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   plugins: ['eslint-plugin-react'],
   env: {
     // 浏览器全局变量
@@ -13,23 +10,23 @@ module.exports = {
     // CommonJS全局变量和CommonJS作用域
     commonjs: true,
     // 启用除模块之外的所有ECMAScript 6功能
-    es6: true,
+    es6: true
   },
 
   parserOptions: {
     sourceType: 'module',
-    ecmaFeatures: { jsx: true },
+    ecmaFeatures: { jsx: true }
   },
 
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+
     // 0:"off", 1:"warn", 2:"error"
     semi: 0,
+    indent: [2, 2],
     '@typescript-eslint/no-empty-interface': 1,
     '@typescript-eslint/no-empty-function': 1,
-    "@typescript-eslint/no-explicit-any": 0,
-    "@typescript-eslint/no-non-null-assertion": 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
     '@typescript-eslint/ban-types': 1,
     'no-use-before-define': 0,
     '@typescript-eslint/no-use-before-define': 2,
@@ -45,7 +42,7 @@ module.exports = {
   },
   settings: {
     react: {
-      version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
+      version: 'detect'
     }
   }
 }
